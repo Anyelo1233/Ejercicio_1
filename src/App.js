2880527fb150a6d5d6b506d888cb0fab7a8aa0ed
Anyelo Gomez Carvajal <anyelogomezcarvajalbedoya@gmail.com>
2021-07-12 14:19:29 -0500

    agregamos ejercicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NuevaTarea from "./Tareas_pendientes/NuevaTarea";
 
 function App() {
+  const tareasUsuario = {
+    tarea: "complementar trabajo",
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NuevaTarea />
     </div>
   );
 }
